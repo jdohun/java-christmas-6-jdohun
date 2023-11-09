f6bc4edf4839858cc222065df22f882c71267040
jdohun <wjdehgns97@naver.com>
2023-11-09 18:32:38 +0900

    Feat: Skeleton Code 생성

---
+++ src/main/java/christmas/constant/DiscountConstant.java
@@ -0,0 +1,9 @@
+package christmas.constant;
+
+public class DiscountConstant {
+    public static final int DAILY_DISCOUNT_AMOUNT = 2_023;
+    public static final int SPECIAL_DISCOUNT_AMOUNT = 1_000;
+    public static final int CHRISTMAS_D_DAY_DISCOUNT_AMOUNT_INITIAL = 1_000;
+    public static final int CHRISTMAS_D_DAY_DISCOUNT_AMOUNT_ADD = 100;
+
+}
