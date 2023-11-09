76af7895e6f1c21417ee9a6c5d37adc58c3925df
DH Jeong <wjdehgns97@gmail.com>
2023-11-09 18:54:47 +0900

    feat: Skeleton Code 생성

---
+++ src/main/java/christmas/constant/RegularConstant.java
@@ -0,0 +1,7 @@
+package christmas.constant;
+
+public class RegularConstant {
+    public static final String INPUT_DELIMITER = ",";
+    public static final String MENU_DELIMITER = "-";
+
+}
---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,14 @@
+package christmas.view;
+
+import christmas.constant.message.OutputMessage;
+
+public class OutputView {
+    private OutputView() {
+    }
+
+    public static void previewEventBenefits(int expectedVisitDay) {
+        System.out.println(OutputMessage
+                .PREVIEW_EVENT_BENEFITS_FORMAT
+                .getIncludingValueMessage(expectedVisitDay));
+    }
+}
