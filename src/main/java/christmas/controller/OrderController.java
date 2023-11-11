b2b5890176bd7f5b933efc1bf6c274096b4b8104
jdohun <wjdehgns97@naver.com>
2023-11-12 01:05:52 +0900

    refactor: 입력 받은 날짜 값을 검증하는 메소드가 매개변수를 받도록 변경
    InputValidator.java
    OrderController.java

--- src/main/java/christmas/controller/OrderController.java
+++ src/main/java/christmas/controller/OrderController.java
@@ -2,11 +2,12 @@ package christmas.controller;
 
 import christmas.domain.validator.InputValidator;
 import christmas.utils.InputSupplier;
+import christmas.view.InputView;
 
 public class OrderController {
 
     public void run() {
-        int expectedVisitDay = getInput(() -> InputValidator.validateInputDay());
+        int expectedVisitDay = getInput(() -> InputValidator.validateInputDay(InputView.inputExpectedVisitDay()));
     }
 
     private <T> T getInput(InputSupplier<T> inputSupplier) {
--- src/main/java/christmas/domain/validator/InputValidator.java
+++ src/main/java/christmas/domain/validator/InputValidator.java
@@ -3,16 +3,14 @@ package christmas.domain.validator;
 import christmas.view.InputView;
 
 public final class InputValidator {
-    public static int validateInputDay() {
-        String inputExpectedVisitDayValue = InputView.inputExpectedVisitDay();
+    public static int validateInputDay(String inputExpectedVisitDayValue) {
         Validator.validateExistValue(inputExpectedVisitDayValue);
         int expectedVisitDay = Validator.validateNumericInput(inputExpectedVisitDayValue);
         Validator.validateDayInRange(expectedVisitDay);
         return expectedVisitDay;
     }
 
-    public static void validateInputOrderList() {
-        String inputExpectedVisitDayValue = InputView.inputOrderList();
+    public static void validateInputOrderList(String inputExpectedVisitDayValue) {
         Validator.validateExistValue(inputExpectedVisitDayValue);
     }
 }
