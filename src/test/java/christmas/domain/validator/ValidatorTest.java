67c46552279994d0f34e329969bc409ec4317dbe
jdohun <wjdehgns97@naver.com>
2023-11-12 01:07:17 +0900

    test: 입력 값 검증 기능 테스트 수행
    ValidatorTest.java

---
+++ src/test/java/christmas/domain/validator/ValidatorTest.java
@@ -0,0 +1,53 @@
+package christmas.domain.validator;
+
+import christmas.constant.message.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ValidatorTest {
+
+    @DisplayName("공백이나 빈값이 입력되면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"", " ", "  ", "   "})
+    void validateExistValue(String whiteSpace) {
+        assertThatThrownBy(() -> Validator.validateExistValue(whiteSpace))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_NOT_EXISTENT_VALUE);
+    }
+
+    @DisplayName("맨 앞자리가 0으로 시작하거나 숫자가 아닌 값이 입력되면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"0", "원", "1000d"})
+    void validateNumericInput(String notNumber) {
+        assertThatThrownBy(() -> Validator.validateNumericInput(notNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_NOT_NUMERIC_VALUE);
+    }
+
+    @DisplayName("쉼표 전후에 아무런 값이 없으면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,,,3,4,5,6", ",,1,2,3,4,5,6", "1,2,3,4,5,6,", "1, , 2, 3, 4, 5, 6"})
+    void hasCommasWithoutSurroundingValues(String valuesSeparatedByCommas) {
+        assertThatThrownBy(() -> Validator.hasCommasWithoutSurroundingValues(valuesSeparatedByCommas))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.ERROR_CONTAIN_CONSECUTIVE_COMMAS);
+    }
+
+    @DisplayName("올바른 값이 들어왔을 때 예외가 발생하지 않는다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2,3,4,5,6", "1, 2, 3, 4, 5, 6", "1, 2, 3 , 4, 5, 6"})
+    void hasCommasWithoutSurroundingValues2(String valuesSeparatedByCommas) {
+        assertThatCode(() -> Validator.hasCommasWithoutSurroundingValues(valuesSeparatedByCommas))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("1~31 에 해당하는 숫자 아니면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, 32, 2023, 2022})
+    void validateDayInRange() {
+    }
+}
