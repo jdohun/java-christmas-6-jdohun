c21e9e1f0f254f5aa5a625c19bed515b5ceca166
jdohun <wjdehgns97@naver.com>
2023-11-11 15:22:17 +0900

    refactor: 상수명 변경
    RegularConstant.java
    INPUT_DELIMITER -> ORDER_LIST_DELIMITER

--- src/main/java/christmas/constant/RegularConstant.java
+++ src/main/java/christmas/constant/RegularConstant.java
@@ -1,7 +1,7 @@
 package christmas.constant;
 
 public class RegularConstant {
-    public static final String INPUT_DELIMITER = ",";
+    public static final String ORDER_LIST_DELIMITER = ",";
     public static final String MENU_DELIMITER = "-";
     public static final int MONTH_FIRST = 1;
     public static final int MONTH_LAST = 31;
