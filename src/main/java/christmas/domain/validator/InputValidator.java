a09d228b67df6a18403bd0f8d0f5218d72d848d8
DH Jeong <wjdehgns97@gmail.com>
2023-11-12 14:17:08 +0900

    feat: 추가된 검증 기능을 사용하여 입력된 주문 값을 검증하는 기능 구현
    InputValidator.java

--- src/main/java/christmas/domain/validator/InputValidator.java
+++ src/main/java/christmas/domain/validator/InputValidator.java
@@ -1,8 +1,14 @@
 package christmas.domain.validator;
 
-import christmas.view.InputView;
+import christmas.constant.Menu;
+import christmas.constant.RegularConstant;
+
+import java.util.HashMap;
 
 public final class InputValidator {
+    private InputValidator() {
+    }
+
     public static int validateInputDay(String inputExpectedVisitDayValue) {
         Validator.validateExistValue(inputExpectedVisitDayValue);
         int expectedVisitDay = Validator.validateNumericInput(inputExpectedVisitDayValue);
@@ -10,7 +16,30 @@ public final class InputValidator {
         return expectedVisitDay;
     }
 
-    public static void validateInputOrderList(String inputExpectedVisitDayValue) {
-        Validator.validateExistValue(inputExpectedVisitDayValue);
+    public static HashMap<Menu, Integer> validateInputOrderValue(String inputOrderMenu) {
+        Validator.validateExistValue(inputOrderMenu);
+        Validator.hasCommasWithoutSurroundingValues(inputOrderMenu);
+        String[] splitInputOrderMenu = inputOrderMenu.split(RegularConstant.ORDER_LIST_DELIMITER);
+
+        return validateOrderPolicy(splitInputOrderMenu);
+    }
+
+    private static HashMap<Menu, Integer> validateOrderPolicy(String[] splitInputOrderMenu) {
+        HashMap<Menu, Integer> orderMenu = new HashMap<>();
+        int menuCount = 0;
+
+        for (String separatedInputMenu : splitInputOrderMenu) {
+            Validator.validateMenuFormat(separatedInputMenu);
+            String[] menuNameWithCount = separatedInputMenu.split(RegularConstant.MENU_DELIMITER);
+            Menu menu = Validator.validateExistMenu(menuNameWithCount[0]);
+            Validator.validateDuplicateMenu(orderMenu, menu);
+            orderMenu.put(menu, Integer.parseInt(menuNameWithCount[1]));
+            menuCount += Integer.parseInt(menuNameWithCount[1]);
+            Validator.validateMenuCount(menuCount);
+        }
+
+        Validator.validateOnlyBeverageOrder(orderMenu);
+
+        return orderMenu;
     }
 }
