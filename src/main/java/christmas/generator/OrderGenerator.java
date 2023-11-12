24f26113c0acd89f55726a1551ff48a37a5f353b
jdohun <wjdehgns97@naver.com>
2023-11-12 14:21:17 +0900

    feat: 주문 정보를 생성하는 Generator 추가, Controller 에서 사용
    OrderGenerator.java

---
+++ src/main/java/christmas/generator/OrderGenerator.java
@@ -0,0 +1,26 @@
+package christmas.generator;
+
+import christmas.constant.Menu;
+import christmas.domain.model.OrderInfo;
+import christmas.domain.validator.InputValidator;
+import christmas.utils.InputSupplier;
+import christmas.view.InputView;
+
+import java.util.HashMap;
+
+public class OrderGenerator {
+    public OrderInfo run() {
+        int expectedVisitDay = getInput(() -> InputValidator.validateInputDay(InputView.inputExpectedVisitDay()));
+        HashMap<Menu, Integer> orderMenu = getInput(() -> InputValidator.validateInputOrderValue(InputView.inputOrderList()));
+
+        return new OrderInfo(expectedVisitDay, orderMenu);
+    }
+
+    private <T> T getInput(InputSupplier<T> inputSupplier) {
+        while (true) try {
+            return inputSupplier.get();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
