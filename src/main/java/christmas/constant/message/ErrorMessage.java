dd054b16e9677ef8fe3c2ffa604b593823d91d5e
DH Jeong <wjdehgns97@gmail.com>
2023-11-12 14:09:26 +0900

    docs: 에러메시지 사용 시기에 대한 설명 주석 추가
    ErrorMessage.java

--- src/main/java/christmas/constant/message/ErrorMessage.java
+++ src/main/java/christmas/constant/message/ErrorMessage.java
@@ -19,6 +19,7 @@ public final class ErrorMessage {
      * 메뉴판에 없는 메뉴를 입력하는 경우
      * 메뉴의 개수에 1 이상의 숫자가 아닌 값이 있는 경우
      * 메뉴 형식이 예시와 다른 경우
+     * 중복 메뉴를 입력한 경우
      */
     public static final String ERROR_INVALID_ORDER = ERROR_MESSAGE_PREFIX + "유효하지 않은 주문입니다. 다시 입력해 주세요.\n";
     public static final String ERROR_OVER_COUNT_OF_MENU = ERROR_MESSAGE_PREFIX + "메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요.\n";
