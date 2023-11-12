bae18cfe5b113174ca9805a9f13ac45b6d6ed24a
jdohun <wjdehgns97@naver.com>
2023-11-12 12:47:48 +0900

    feat: 메뉴 카테고리를 관리하는 상수 클래스 생성
    MenuCategory.java

---
+++ src/main/java/christmas/constant/MenuCategory.java
@@ -0,0 +1,8 @@
+package christmas.constant;
+
+public final class MenuCategory {
+    public static final String APPETIZER = "애피타이저";
+    public static final String MAIN = "메인";
+    public static final String DESSERT = "디저트";
+    public static final String BEVERAGE = "음료";
+}
