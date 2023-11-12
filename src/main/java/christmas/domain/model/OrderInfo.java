5dcaca1f1718a07061c14a2358f96c3b5129706a
DH Jeong <wjdehgns97@gmail.com>
2023-11-12 14:15:03 +0900

    docs: 예상 방문 날짜와 주문 메뉴를 저장할 Class 뼈대 생성
    OrderInfo.java

--- src/main/java/christmas/domain/model/OrderInfo.java
+++ src/main/java/christmas/domain/model/OrderInfo.java
@@ -1,4 +1,15 @@
 package christmas.domain.model;
 
+import christmas.constant.Menu;
+
+import java.util.HashMap;
+
 public class OrderInfo {
+    private int expectedVisitDay;
+    private HashMap<Menu, Integer> orderMenu;
+
+    public OrderInfo(int expectedVisitDay, HashMap orderMenu) {
+        this.expectedVisitDay = expectedVisitDay;
+        this.orderMenu = orderMenu;
+    }
 }
