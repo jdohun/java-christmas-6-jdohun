4240909b153ac492cdc868973266b8f6c40bf1c0
DH Jeong <wjdehgns97@gmail.com>
2023-11-12 12:56:42 +0900

    feat: 메뉴 검증할 때 사용하는 에러메시지 추가
    ErrorMessage.java

--- src/main/java/christmas/constant/message/ErrorMessage.java
+++ src/main/java/christmas/constant/message/ErrorMessage.java
@@ -18,4 +18,6 @@ public final class ErrorMessage {
      * 메뉴 형식이 예시와 다른 경우
      */
     public static final String ERROR_INVALID_ORDER = ERROR_MESSAGE_PREFIX + "유효하지 않은 주문입니다. 다시 입력해 주세요.\n";
+    public static final String ERROR_OVER_COUNT_OF_MENU = ERROR_MESSAGE_PREFIX + "메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요.\n";
+    public static final String ERROR_ORDER_ONLY_DRINK = ERROR_MESSAGE_PREFIX + "음료만 주문 시, 주문할 수 없습니다. 다시 입력해 주세요.\n";
 }
