9c709393ceea7a46f764a23bcc52a6e0d1215e52
DH Jeong <wjdehgns97@gmail.com>
2023-11-12 13:46:50 +0900

    refactor: 상수 클래스 생성자 접근 제한, 상속 제한 추가
    InputMessage.java
    ErrorMessage.java
    MenuCategory.java
    RegularConstant.java
    PatternConstant.java
    DiscountConstant.java
    Badge.java

--- src/main/java/christmas/constant/MenuCategory.java
+++ src/main/java/christmas/constant/MenuCategory.java
@@ -1,6 +1,9 @@
 package christmas.constant;
 
 public final class MenuCategory {
+    private MenuCategory() {
+    }
+
     public static final String APPETIZER = "애피타이저";
     public static final String MAIN = "메인";
     public static final String DESSERT = "디저트";
--- src/main/java/christmas/constant/PatternConstant.java
+++ src/main/java/christmas/constant/PatternConstant.java
@@ -3,6 +3,9 @@ package christmas.constant;
 import java.util.regex.Pattern;
 
 public final class PatternConstant {
+    private PatternConstant() {
+    }
+
     public static final Pattern NUMBER_PATTERN = Pattern.compile("^[1-9][0-9]*");
     public static final Pattern HAS_COMMAS_WITHOUT_SURROUNDING_VALUES_PATTERNS = Pattern.compile("^\\s*,|,\\s*,|,\\s*$");
     public static final Pattern MENU_PATTERN = Pattern.compile("[가-힣]+-[1-9][0-9]*");
