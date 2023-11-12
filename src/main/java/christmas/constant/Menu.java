c01d0120aad94786f339c43a612e4a88414fcb98
jdohun <wjdehgns97@naver.com>
2023-11-12 12:56:32 +0900

    refactor: 메뉴 카테고리 상수를 이용하여 메뉴 관리
    Menu.java

--- src/main/java/christmas/constant/Menu.java
+++ src/main/java/christmas/constant/Menu.java
@@ -14,22 +14,21 @@ public enum Menu {
     <음료>
     제로콜라(3,000), 레드와인(60,000), 샴페인(25,000)
      */
+    BUTTON_MUSHROOM_SOUP(MenuCategory.APPETIZER, "양송이수프", 6_000),
+    TAPAS(MenuCategory.APPETIZER, "타파스", 5_500),
+    CAESAR_SALAD(MenuCategory.APPETIZER, "시저샐러드", 8_000),
 
-    BUTTON_MUSHROOM_SOUP("애피타이저", "양송이수프", 6_000),
-    TAPAS("애피타이저", "타파스", 5_500),
-    CAESAR_SALAD("애피타이저", "시저샐러드", 8_000),
+    T_BONE_STEAK(MenuCategory.MAIN, "티본스테이크", 55_000),
+    BARBECUE_RIBS(MenuCategory.MAIN, "바비큐립", 54_000),
+    SEAFOOD_PASTA(MenuCategory.MAIN, "해산물파스타", 35_000),
+    CHRISTMAS_PASTA(MenuCategory.MAIN, "크리스마스파스타", 25_000),
 
-    T_BONE_STEAK("메인", "티본스테이크", 55_000),
-    BARBECUE_RIBS("메인", "바비큐립", 54_000),
-    SEAFOOD_PASTA("메인", "해산물파스타", 35_000),
-    CHRISTMAS_PASTA("메인", "크리스마스파스타", 25_000),
+    CHOCOLATE_CAKE(MenuCategory.DESSERT, "초코케이크", 15_000),
+    ICE_CREAM(MenuCategory.DESSERT, "아이스크림", 5_000),
 
-    CHOCOLATE_CAKE("디저트", "초코케이크", 15_000),
-    ICE_CREAM("디저트", "아이스크림", 5_000),
-
-    ZERO_COKE("음료", "제로콜라", 3_000),
-    RED_WINE("음료", "레드와인", 60_000),
-    CHAMPAGNE("음료", "샴페인", 25_000);
+    ZERO_COKE(MenuCategory.BEVERAGE, "제로콜라", 3_000),
+    RED_WINE(MenuCategory.BEVERAGE, "레드와인", 60_000),
+    CHAMPAGNE(MenuCategory.BEVERAGE, "샴페인", 25_000);
 
     private final String category;
     private final String name;
