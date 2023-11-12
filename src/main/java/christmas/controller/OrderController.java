b5cf98d312db6b5005f824584811df494fc7ce80
jdohun <wjdehgns97@naver.com>
2023-11-12 17:17:44 +0900

    refactor: 주문 생성기로부터 반환된 주문정보 객체를 받는 변수 추가
    OrderController.java

--- src/main/java/christmas/controller/OrderController.java
+++ src/main/java/christmas/controller/OrderController.java
@@ -1,12 +1,13 @@
 package christmas.controller;
 
+import christmas.domain.model.OrderInfo;
 import christmas.generator.OrderGenerator;
 
 public class OrderController {
     private final OrderGenerator orderGenerator = new OrderGenerator();
 
     public void run() {
-        orderGenerator.run();
+        OrderInfo orderInfo = orderGenerator.run();
     }
 
 }
