5ee8a731e8faaa26768db379fc825d830bde8684
DH Jeong <wjdehgns97@gmail.com>
2023-11-13 22:48:05 +0900

    feat: 주문 메뉴를 보여주는 포맷 추가
    OutputMessage.java

--- src/main/java/christmas/constant/message/OutputMessage.java
+++ src/main/java/christmas/constant/message/OutputMessage.java
@@ -1,7 +1,8 @@
 package christmas.constant.message;
 
 public enum OutputMessage {
-    PREVIEW_EVENT_BENEFITS_FORMAT("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n");
+    PREVIEW_EVENT_BENEFITS_FORMAT("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n"),
+    PREVIEW_ORDER_MENU_FORMAT("%s %d개");
 
     private final String messageFormat;
 
@@ -13,7 +14,11 @@ public enum OutputMessage {
         return this.messageFormat;
     }
 
-    public String getIncludingValueMessage(int value){
+    public String getIncludingAnIntMessage(int value) {
         return String.format(this.messageFormat, value);
+    }
+
+    public String getOrderMenuFormat(String menuName, int countOfMenu) {
+        return String.format(this.messageFormat, menuName, countOfMenu);
     }
 }
