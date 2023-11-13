ff3bf24d02794272d83133e6062cff6be17f0743
jdohun <wjdehgns97@naver.com>
2023-11-13 22:51:09 +0900

    feat: 날짜를 포함한 미리보기 제목을 반환하는 기능, 메뉴를 보여주는 기능, 할인 전 총금액을 계산하는 기능 구현
    OrderInfo.java

--- src/main/java/christmas/domain/model/OrderInfo.java
+++ src/main/java/christmas/domain/model/OrderInfo.java
@@ -1,8 +1,10 @@
 package christmas.domain.model;
 
 import christmas.constant.Menu;
+import christmas.constant.message.OutputMessage;
 import christmas.domain.validator.Validator;
 
+import java.util.Collection;
 import java.util.HashMap;
 
 public class OrderInfo {
@@ -17,33 +19,51 @@ public class OrderInfo {
         this.orderMenu = orderMenu;
     }
 
-    // 생성 전에 날짜가 1~31 사이인지 검증
-    // 생성 전에 메뉴의 총개수가 20 이하인지 검증
+    public String getIncludingExpectedVisitDayPreviewTitle() {
+        return OutputMessage
+                .PREVIEW_EVENT_BENEFITS_FORMAT
+                .getIncludingAnIntMessage(expectedVisitDay);
+    }
 
-    // 주문 메뉴 백브리핑
+    public void showMenu() {
+        orderMenu.forEach((key, value) -> {
+            System.out.println(
+                    OutputMessage
+                            .PREVIEW_ORDER_MENU_FORMAT
+                            .getOrderMenuFormat(key.getName(), value)
+            );
+        });
+    }
 
-    // 할인 전 총 주문 금액 계산
+    public int calculateTotalAmountBeforeDiscount() {
+        int totalAmountBeforeDiscount = 0;
+        Collection<Integer> values = orderMenu.values();
+        for (int value : values) {
+            totalAmountBeforeDiscount += value;
+        }
+        return totalAmountBeforeDiscount;
+    }
 
     // 크리스마스 디데이 할인
-        // 총주문 금액이 10,000원 이상인지 확인
-        // 크리스마스 디데이 기간에 해당하는 날짜인지 확인
-        // 해당하면 할인 금액 계산
+    // 총주문 금액이 10,000원 이상인지 확인
+    // 크리스마스 디데이 기간에 해당하는 날짜인지 확인
+    // 해당하면 할인 금액 계산
 
 
     // 12월 이벤트 할인
-        // 총주문 금액이 10,000원 이상인지 확인
-        // 평일인지 주말인지 별이 있는 날인지 확인
-        // 해당하는 할인 혜택 적용
+    // 총주문 금액이 10,000원 이상인지 확인
+    // 평일인지 주말인지 별이 있는 날인지 확인
+    // 해당하는 할인 혜택 적용
 
     // 증정이벤트
-        // 총주문 금액이 120,000원 이상인지 확인
-        // 해당하면 샴페인 증정
-        // 샴페인이 주문내역에 존재하는지 확인
-        // 존재하면 +1 아니면 추가
-    
+    // 총주문 금액이 120,000원 이상인지 확인
+    // 해당하면 샴페인 증정
+    // 샴페인이 주문내역에 존재하는지 확인
+    // 존재하면 +1 아니면 추가
+
     // 배지 부여
-        // 총혜택 금액 계산
-        // 크리스마스 디데이 할인 + 이벤트 할인 + 증정이벤트(샴페인-25,000)
-        // 기준에 따라 배지부여
+    // 총혜택 금액 계산
+    // 크리스마스 디데이 할인 + 이벤트 할인 + 증정이벤트(샴페인-25,000)
+    // 기준에 따라 배지부여
 
 }
