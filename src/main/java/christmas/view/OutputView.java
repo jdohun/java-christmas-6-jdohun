90f2de5cea82bdce1d4b47bc6da1cc589616e34a
DH Jeong <wjdehgns97@gmail.com>
2023-11-14 01:23:45 +0900

    feat: 혜택 미리 보기 타이틀, 주문 메뉴, 할인 전 총주문 금액, 증정 메뉴 를 출력하는 기능 구현
    OutputView.java

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,14 +1,30 @@
 package christmas.view;
 
+import christmas.constant.Menu;
+import christmas.constant.PreviewTitle;
 import christmas.constant.message.OutputMessage;
+import christmas.domain.model.OrderInfo;
+
+import java.util.AbstractMap;
 
 public final class OutputView {
     private OutputView() {
     }
 
-    public static void previewEventBenefits(int expectedVisitDay) {
-        System.out.println(OutputMessage
-                .PREVIEW_EVENT_BENEFITS_FORMAT
-                .getIncludingValueMessage(expectedVisitDay));
+    public static void previewEventBenefits(OrderInfo orderInfo) {
+        AbstractMap.SimpleEntry<Menu, Integer> giveawayMenu = orderInfo.getGiveawayMenu();
+
+        System.out.println(orderInfo.getIncludingExpectedVisitDayPreviewTitle());
+        System.out.println(PreviewTitle.ORDER_MENU);
+        orderInfo.showMenu();
+        System.out.println(PreviewTitle.TOTAL_PRICE_BEFORE_DISCOUNT);
+        System.out.println(OutputMessage.OUTPUT_AMOUNT_FORMAT
+                .getAmountFormat(orderInfo.getTotalAmountBeforeDiscount()) + "\n");
+        System.out.println(PreviewTitle.GIVEAWAY_MENU);
+        System.out.println(
+                OutputMessage
+                        .PREVIEW_ORDER_MENU_FORMAT
+                        .getOrderMenuFormat(giveawayMenu.getKey().getName(), giveawayMenu.getValue()) + "\n"
+        );
     }
 }
