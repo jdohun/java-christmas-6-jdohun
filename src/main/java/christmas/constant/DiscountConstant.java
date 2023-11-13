75b4705640ce3aa97fb4e779490ce38c4678553e
DH Jeong <wjdehgns97@gmail.com>
2023-11-14 01:28:55 +0900

    refactor: 할인 금액 상수를 음수로 변경
    Menu.java
    DiscountConstant.java

--- src/main/java/christmas/constant/DiscountConstant.java
+++ src/main/java/christmas/constant/DiscountConstant.java
@@ -4,9 +4,9 @@ public final class DiscountConstant {
     private DiscountConstant() {
     }
 
-    public static final int DAILY_DISCOUNT_AMOUNT = 2_023;
-    public static final int SPECIAL_DISCOUNT_AMOUNT = 1_000;
-    public static final int CHRISTMAS_D_DAY_DISCOUNT_AMOUNT_INITIAL = 1_000;
-    public static final int CHRISTMAS_D_DAY_DISCOUNT_AMOUNT_ADD = 100;
+    public static final int DAILY_DISCOUNT_AMOUNT = -2_023;
+    public static final int SPECIAL_DISCOUNT_AMOUNT = -1_000;
+    public static final int CHRISTMAS_D_DAY_DISCOUNT_AMOUNT_INITIAL = -1_000;
+    public static final int CHRISTMAS_D_DAY_DISCOUNT_AMOUNT_ADD = -100;
 
 }
