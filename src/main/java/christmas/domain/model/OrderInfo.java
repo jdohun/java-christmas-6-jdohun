559b9c9439900e68248d72e3a8e07087d6f42917
jdohun <wjdehgns97@naver.com>
2023-11-14 01:04:03 +0900

    feat: 할인 전 총금액을 계산하는 기능, 이벤트 자격을 확인하는 기능, 증정 이벤트에 당첨됐는지 확인하는 기능, 크리스마스 디데이 할인 금액을 계산하는 기능 추가
    OrderInfo.java
    calculateTotalAmountBeforeDiscount()
    checkEventEligibility()
    checkGiveawayEventEligibility()
    calculateChristmasDdayDiscountAmount()

--- src/main/java/christmas/domain/model/OrderInfo.java
+++ src/main/java/christmas/domain/model/OrderInfo.java
@@ -1,15 +1,22 @@
 package christmas.domain.model;
 
+import christmas.constant.DiscountConstant;
+import christmas.constant.EventConstant;
 import christmas.constant.Menu;
 import christmas.constant.message.OutputMessage;
 import christmas.domain.validator.Validator;
 
+import java.util.AbstractMap;
 import java.util.Collection;
 import java.util.HashMap;
 
 public class OrderInfo {
-    private int expectedVisitDay;
-    private HashMap<Menu, Integer> orderMenu;
+    private final int expectedVisitDay;
+    private final HashMap<Menu, Integer> orderMenu;
+    private final int totalAmountBeforeDiscount;
+    private final boolean eventEligibility;
+    private final AbstractMap.SimpleEntry<Menu, Integer> giveawayMenu;
+
 
     public OrderInfo(int expectedVisitDay, HashMap orderMenu) {
         Validator.validateDayInRange(expectedVisitDay);
@@ -17,12 +24,34 @@ public class OrderInfo {
 
         this.expectedVisitDay = expectedVisitDay;
         this.orderMenu = orderMenu;
+        this.totalAmountBeforeDiscount = calculateTotalAmountBeforeDiscount();
+        this.eventEligibility = checkEventEligibility();
+        this.giveawayMenu = checkGiveawayEventEligibility();
+
     }
+
+    public int getTotalAmountBeforeDiscount() {
+        return totalAmountBeforeDiscount;
+    }
+
+    public AbstractMap.SimpleEntry<Menu, Integer> getGiveawayMenu() {
+        return giveawayMenu;
+    }
+
 
     public String getIncludingExpectedVisitDayPreviewTitle() {
         return OutputMessage
                 .PREVIEW_EVENT_BENEFITS_FORMAT
                 .getIncludingAnIntMessage(expectedVisitDay);
+    }
+
+    private int calculateTotalAmountBeforeDiscount() {
+        int totalAmountBeforeDiscount = 0;
+        Collection<Integer> values = orderMenu.values();
+        for (int value : values) {
+            totalAmountBeforeDiscount += value;
+        }
+        return totalAmountBeforeDiscount;
     }
 
     public void showMenu() {
@@ -32,34 +61,38 @@ public class OrderInfo {
                             .PREVIEW_ORDER_MENU_FORMAT
                             .getOrderMenuFormat(key.getName(), value)
             );
+            System.out.println();
         });
     }
 
-    public int calculateTotalAmountBeforeDiscount() {
-        int totalAmountBeforeDiscount = 0;
-        Collection<Integer> values = orderMenu.values();
-        for (int value : values) {
-            totalAmountBeforeDiscount += value;
+    // 할인 전 총주문 금액이 10,000원 이상인지 확인
+    private boolean checkEventEligibility() {
+        if (totalAmountBeforeDiscount >= EventConstant.EVENT_ELIGIBILITY_AMOUNT) {
+            return true;
         }
-        return totalAmountBeforeDiscount;
+        return false;
     }
 
-    // 크리스마스 디데이 할인
-    // 총주문 금액이 10,000원 이상인지 확인
-    // 크리스마스 디데이 기간에 해당하는 날짜인지 확인
-    // 해당하면 할인 금액 계산
+    // 증정이벤트
+    private AbstractMap.SimpleEntry<Menu, Integer> checkGiveawayEventEligibility() {
+        if (eventEligibility && totalAmountBeforeDiscount >= EventConstant.EVENT_GIVEAWAY_ELIGIBILITY_AMOUNT) {
+            return new AbstractMap.SimpleEntry<>(Menu.GIVEAWAY_CHAMPAGNE, 1);
+        }
+        return new AbstractMap.SimpleEntry<>(Menu.GIVEAWAY_NONE, 0);
+    }
 
+    private int calculateChristmasDdayDiscountAmount() {
+        if (eventEligibility && expectedVisitDay <= EventConstant.CHRISTMAS) {
+            return DiscountConstant.CHRISTMAS_D_DAY_DISCOUNT_AMOUNT_INITIAL
+                    + DiscountConstant.CHRISTMAS_D_DAY_DISCOUNT_AMOUNT_ADD * (expectedVisitDay-1);
+        }
+        return 0;
+    }
 
     // 12월 이벤트 할인
-    // 총주문 금액이 10,000원 이상인지 확인
+    // 할인 전 총주문 금액이 10,000원 이상인지 확인
     // 평일인지 주말인지 별이 있는 날인지 확인
     // 해당하는 할인 혜택 적용
-
-    // 증정이벤트
-    // 총주문 금액이 120,000원 이상인지 확인
-    // 해당하면 샴페인 증정
-    // 샴페인이 주문내역에 존재하는지 확인
-    // 존재하면 +1 아니면 추가
 
     // 배지 부여
     // 총혜택 금액 계산
