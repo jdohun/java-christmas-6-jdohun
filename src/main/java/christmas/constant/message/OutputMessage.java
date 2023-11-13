8413c4684ae316fdd8fa6b6838bf56ce8c705450
jdohun <wjdehgns97@naver.com>
2023-11-13 23:42:18 +0900

    feat: 금액 출력 포맷 상수 추가
    OutputMessage.java

--- src/main/java/christmas/constant/message/OutputMessage.java
+++ src/main/java/christmas/constant/message/OutputMessage.java
@@ -2,7 +2,8 @@ package christmas.constant.message;
 
 public enum OutputMessage {
     PREVIEW_EVENT_BENEFITS_FORMAT("12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n"),
-    PREVIEW_ORDER_MENU_FORMAT("%s %d개");
+    PREVIEW_ORDER_MENU_FORMAT("%s %d개"),
+    OUTPUT_AMOUNT_FORMAT("%,d원");
 
     private final String messageFormat;
 
@@ -20,5 +21,9 @@ public enum OutputMessage {
 
     public String getOrderMenuFormat(String menuName, int countOfMenu) {
         return String.format(this.messageFormat, menuName, countOfMenu);
+    }
+
+    public String getAmountFormat(int money) {
+        return String.format(this.messageFormat, money);
     }
 }
