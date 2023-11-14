1a792f3081023f8910ebd9e0ce1215d8b6d01799
DH Jeong <wjdehgns97@gmail.com>
2023-11-14 12:44:25 +0900

    feat: 미리보기 출력 메소드를 제목 별로 분리
    OutputView.java
    previewEventBenefits()
    showPreviewTitle()
    showPreviewOrderMenu()
    showPreviewTotalPriceBeforeDiscount()
    showPreviewGiveawayMenu()
    showBenefitDetail()
    showTotalBenefitAmount()

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -11,20 +11,54 @@ public final class OutputView {
     private OutputView() {
     }
 
-    public static void previewEventBenefits(OrderInfo orderInfo) {
-        AbstractMap.SimpleEntry<Menu, Integer> giveawayMenu = orderInfo.getGiveawayMenu();
-
+    public static void showPreviewTitle(OrderInfo orderInfo) {
         System.out.println(orderInfo.getIncludingExpectedVisitDayPreviewTitle());
+    }
+
+    public static void showPreviewOrderMenu(OrderInfo orderInfo) {
         System.out.println(PreviewTitle.ORDER_MENU);
         orderInfo.showMenu();
+    }
+
+    public static void showPreviewTotalPriceBeforeDiscount(OrderInfo orderInfo) {
         System.out.println(PreviewTitle.TOTAL_PRICE_BEFORE_DISCOUNT);
-        System.out.println(OutputMessage.OUTPUT_AMOUNT_FORMAT
-                .getAmountFormat(orderInfo.getTotalAmountBeforeDiscount()) + "\n");
+        System.out.println(
+                OutputMessage.OUTPUT_AMOUNT_FORMAT
+                .getAmountFormat(orderInfo.getTotalAmountBeforeDiscount()) + "\n"
+        );
+    }
+
+    public static void showPreviewGiveawayMenu(OrderInfo orderInfo) {
+        AbstractMap.SimpleEntry<Menu, Integer> giveawayMenu = orderInfo.getGiveawayMenu();
         System.out.println(PreviewTitle.GIVEAWAY_MENU);
         System.out.println(
                 OutputMessage
                         .PREVIEW_ORDER_MENU_FORMAT
                         .getOrderMenuFormat(giveawayMenu.getKey().getName(), giveawayMenu.getValue()) + "\n"
         );
+    }
+
+    public static void showBenefitDetail(OrderInfo orderInfo) {
+        System.out.println(PreviewTitle.BENEFIT_DETAILS);
+        orderInfo.showBenefitDetails();
+    }
+
+    public static void showTotalBenefitAmount(OrderInfo orderInfo) {
+        System.out.println(PreviewTitle.TOTAL_BENEFIT_AMOUNT);
+        System.out.println(
+                OutputMessage
+                        .OUTPUT_AMOUNT_FORMAT
+                        .getAmountFormat(orderInfo.calculateBenefitAmount()) + "\n"
+        );
+    }
+
+    public static void previewEventBenefits(OrderInfo orderInfo) {
+        showPreviewTitle(orderInfo);
+        showPreviewOrderMenu(orderInfo);
+        showPreviewTotalPriceBeforeDiscount(orderInfo);
+        showPreviewGiveawayMenu(orderInfo);
+        showBenefitDetail(orderInfo);
+        showTotalBenefitAmount(orderInfo);
+
     }
 }
