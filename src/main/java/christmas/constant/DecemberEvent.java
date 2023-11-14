8993dfa022fb5d74aebbb1f27b0194f5cf4b3e8e
jdohun <wjdehgns97@naver.com>
2023-11-14 11:56:38 +0900

    feat: 12월 이벤트 관련 상수 클래스 이름 변경 및 상수 추가
    EventConstant -> DecemberEvent
    DecemberEvent.java

---
+++ src/main/java/christmas/constant/DecemberEvent.java
@@ -0,0 +1,19 @@
+package christmas.constant;
+
+public final class DecemberEvent {
+    private DecemberEvent() {
+    }
+
+    public static final int EVENT_ELIGIBILITY_AMOUNT = 10_000;
+    public static final int EVENT_GIVEAWAY_ELIGIBILITY_AMOUNT = 120_000;
+    public static final int FRIDAY = 1;
+    public static final int SATURDAY = 2;
+    public static final int SUNDAY = 3;
+    public static final int CHRISTMAS = 25;
+    public static final String NONE = "없음";
+    public static final String CHRISTMAS_D_DAY_DISCOUNT = "크리스마스 디데이 할인";
+    public static final String WEEKDAY_DISCOUNT = "평일 할인";
+    public static final String WEEKEND_DISCOUNT = "주말 할인";
+    public static final String SPECIAL_DISCOUNT = "특별 할인";
+    public static final String GIVEAWAY_EVENT = "증정 이벤트";
+}
