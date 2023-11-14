aa7908268a145da41a0fc6ba4c5bcf42256858c2
jdohun <wjdehgns97@naver.com>
2023-11-14 13:32:46 +0900

    feat: 추가된 미리보기 출력 기능 사용
    OrderController.java

--- src/main/java/christmas/controller/OrderController.java
+++ src/main/java/christmas/controller/OrderController.java
@@ -2,12 +2,14 @@ package christmas.controller;
 
 import christmas.domain.model.OrderInfo;
 import christmas.generator.OrderGenerator;
+import christmas.view.OutputView;
 
 public class OrderController {
     private final OrderGenerator orderGenerator = new OrderGenerator();
 
     public void run() {
         OrderInfo orderInfo = orderGenerator.run();
+        OutputView.previewEventBenefits(orderInfo);
     }
 
 }
